d9a64d6b4df7c14d2c346a1746822f885a3c848d
Ben Posch <aaronposch4@gmail.com>
2014-05-20 00:30:38 -0700

    initial commit

---
+++ ReadInput/src/ReadInput.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ReadInput.cpp
+// Author      : Ben Posch
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Please enter return" << endl; // prints Please enter return
+	return 0;
+}
